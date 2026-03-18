aa712d96cf1a696d5d72b1aabd7c809af9b84e43
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-03-18 21:45:40 +0100

    implemented basic UserService

---
+++ APBD_cw2_project_s27023/services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using APBD_cw2_project_s27023.modules.user;
+
+namespace APBD_cw2_project_s27023.services;
+
+public class UserService
+{
+    private List<User> _users = [];
+
+    private UserService()
+    {
+    }
+
+    public static UserService Instance { get; } = new();
+
+    public void Add(User user)
+    {
+        _users.Add(user);
+    }
+    
+    public User? Get(int id)
+    {
+        return _users.Find(u => u.Id == id);
+    }
+}
