5664d0ce9ce62ae3cc99c8815d369b9d508107f7
s27023 <s27023@pjwstk.edu.pl>
2026-03-18 19:22:54 +0100

    changed Equipment EquipmentType to getter only and abstract

--- APBD_cw2_project_s27023/modules/equipment/Equipment.cs
+++ APBD_cw2_project_s27023/modules/equipment/Equipment.cs
@@ -4,5 +4,5 @@ namespace APBD_cw2_project_s27023.modules.equipment;
 
 public abstract class Equipment(EquipmentType equipmentType)
 {
-    public EquipmentType Type { get; set; }
+    public abstract EquipmentType Type { get; }
 }
