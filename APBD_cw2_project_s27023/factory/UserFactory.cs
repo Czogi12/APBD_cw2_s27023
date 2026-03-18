dd4a67db6f0428dd3036387a548ce7448aa17a1c
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-03-18 22:13:25 +0100

    implemented basic UserFactory

---
+++ APBD_cw2_project_s27023/factory/UserFactory.cs
@@ -0,0 +1,16 @@
+﻿using APBD_cw2_project_s27023.modules.user;
+
+namespace APBD_cw2_project_s27023.factory;
+
+public class UserFactory
+{
+    public static User? CreateUser(string type, string firstName, string lastName)
+    {
+        return type switch
+        {
+            "student" => new StudentUser(User.GetNextId(), firstName, lastName),
+            "employee" => new EmployeeUser(User.GetNextId(), firstName, lastName),
+            _ => null
+        };
+    }
+}
