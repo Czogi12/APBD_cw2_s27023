b9015b8e9edeafc1d9ea8680e785051d16fed3f8
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-03-20 20:01:28 +0100

    implemented remove user command

---
+++ APBD_cw2_project_s27023/cli/commands/RemoveUserCommand.cs
@@ -0,0 +1,16 @@
+﻿using APBD_cw2_project_s27023.cli.commands.arguments;
+using APBD_cw2_project_s27023.services;
+
+namespace APBD_cw2_project_s27023.cli.commands;
+
+public class RemoveUserCommand() : Command(["remove-user", "ru"],
+    [
+        new IntArgument("id", true, null, null)
+    ],
+    "Adds new user with given FirstName and LastName.")
+{
+    protected override void ExecuteCommand(string[] args)
+    {
+        UserService.Instance.Remove(int.Parse(args[0]));
+    }
+}
