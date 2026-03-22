34c36938b2026deafe9dada00f6657fee5443322
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-03-22 19:57:37 +0100

    implemented servicing service interface

---
+++ APBD_cw2_project_s27023/services/servicing/IServicingService.cs
@@ -0,0 +1,11 @@
+﻿using APBD_cw2_project_s27023.modules.equipment;
+using APBD_cw2_project_s27023.modules.quotable.servicing;
+using APBD_cw2_project_s27023.modules.user;
+
+namespace APBD_cw2_project_s27023.services.servicing;
+
+public interface IServicingService : IService<long, Servicing>
+{
+    public List<Servicing> FindUnpaidServicingOfUser(User user);
+    public Servicing? FindActiveServicingOfEquipment(Equipment equipment);
+}
