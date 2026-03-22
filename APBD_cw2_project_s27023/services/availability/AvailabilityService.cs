3db1854c64572283e6f2a6862bf6551b57bfc918
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-03-22 20:00:29 +0100

    updated availibility service

--- APBD_cw2_project_s27023/services/availability/AvailabilityService.cs
+++ APBD_cw2_project_s27023/services/availability/AvailabilityService.cs
@@ -1,13 +1,14 @@
 ﻿using APBD_cw2_project_s27023.modules.equipment;
 using APBD_cw2_project_s27023.services.rent;
+using APBD_cw2_project_s27023.services.servicing;
 
 namespace APBD_cw2_project_s27023.services.availability;
 
-public class AvailabilityService(IRentService rentService) : IAvailabilityService
+public class AvailabilityService(IRentService rentService, IServicingService servicingService) : IAvailabilityService
 {
     public bool IsAvailable(Equipment equipment)
     {
-        // TODO: add Servicing/Broken status
-        return rentService.FindActiveRentByEquipment(equipment) is null;
+        return rentService.FindActiveRentByEquipment(equipment) is null &&
+               servicingService.FindActiveServicingOfEquipment(equipment) is null;
     }
 }
