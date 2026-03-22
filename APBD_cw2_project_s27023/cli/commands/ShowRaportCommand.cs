24ddfa18119f2afdeab423e0eb6af610d8f25fc3
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-03-22 23:29:38 +0100

    added stale type for raport

--- APBD_cw2_project_s27023/cli/commands/ShowRaportCommand.cs
+++ APBD_cw2_project_s27023/cli/commands/ShowRaportCommand.cs
@@ -1,4 +1,5 @@
-﻿using APBD_cw2_project_s27023.enums;
+﻿using APBD_cw2_project_s27023.cli.commands.arguments;
+using APBD_cw2_project_s27023.enums;
 using APBD_cw2_project_s27023.services;
 using APBD_cw2_project_s27023.services.availability;
 using APBD_cw2_project_s27023.services.equipment;
@@ -7,12 +8,24 @@ using APBD_cw2_project_s27023.services.user;
 
 namespace APBD_cw2_project_s27023.cli.commands;
 
-public class ShowRaportCommand(IUserService userService, IEquipmentService equipmentService, IRentService rentService, IAvailabilityService availabilityService) : Command(["show-raport" ,"raport"], [], "Displays raport")
+public class ShowRaportCommand(IUserService userService, IEquipmentService equipmentService, IRentService rentService,
+    IAvailabilityService availabilityService) : Command(["show-raport" ,"raport"], [
+new StringArgument("type", false, "(a|all|s|stale|)")
+], "Displays raport")
 {
     protected override void ExecuteCommand(string[] args)
     {
-        PrintCountableService("Users", userService);
-        PrintCountableService("Equipment", equipmentService);
+        char raportType = (args.Length > 0 ? args[0] : "all")[0];
+
+        switch (raportType)
+        {
+            case 'a':
+                PrintCountableService("Users", userService);
+                PrintCountableService("Equipment", equipmentService);
+                break;
+            case 's':
+                break;
+        }
     }
     
     private static void PrintCountableService<T>(string mainName, ICountableService<T> countableService) where T : struct, Enum
