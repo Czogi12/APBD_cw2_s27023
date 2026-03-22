625bc8bba0b274ce65e9b4515f83563bc876b090
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-03-22 20:30:21 +0100

    implemented servicing service

--- APBD_cw2_project_s27023/Program.cs
+++ APBD_cw2_project_s27023/Program.cs
@@ -1,20 +1,21 @@
 ﻿using APBD_cw2_project_s27023.cli;
-using APBD_cw2_project_s27023.services;
 using APBD_cw2_project_s27023.services.availability;
 using APBD_cw2_project_s27023.services.equipment;
 using APBD_cw2_project_s27023.services.rent;
+using APBD_cw2_project_s27023.services.servicing;
 using APBD_cw2_project_s27023.services.user;
 
 namespace APBD_cw2_project_s27023;
 
-public class Program
+public static class Program
 {
     public static void Main(string[] args)
     {
         var userService = new UserService();
         var equipmentService = new EquipmentService();
+        var servicingService = new ServicingService();
         var rentService = new RentService(userService, equipmentService);
-        var availabilityService = new AvailabilityService(rentService);
+        var availabilityService = new AvailabilityService(rentService, servicingService);
 
         if (args.Any(arg => arg == "--console"))
             new Cli(userService, equipmentService, rentService, availabilityService);
---
+++ APBD_cw2_project_s27023/services/servicing/ServicingService.cs
@@ -0,0 +1,24 @@
+﻿using APBD_cw2_project_s27023.modules.equipment;
+using APBD_cw2_project_s27023.modules.quotable.servicing;
+using APBD_cw2_project_s27023.modules.user;
+
+namespace APBD_cw2_project_s27023.services.servicing;
+
+public class ServicingService : ServiceWithCache<long, Servicing>, IServicingService
+{
+    public override Servicing Get(long id)
+    {
+        return GetOrDefault(id) ?? throw new ArgumentException("No servicing found for id " + id);
+    }
+
+    public List<Servicing> FindUnpaidServicingOfUser(User user)
+    {
+        return GetAll().FindAll(servicing =>
+            servicing.Rent.User == user && servicing.IsServiced() && !servicing.IsPaidOff);
+    }
+
+    public Servicing? FindActiveServicingOfEquipment(Equipment equipment)
+    {
+        return GetAll().Find(servicing => !servicing.IsServiced() && servicing.Rent.Equipment == equipment);
+    }
+}
