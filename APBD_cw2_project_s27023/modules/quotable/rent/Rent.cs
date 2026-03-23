351caa6155dc603ff4d546d3f02868130b7a043f
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-03-23 11:17:17 +0100

    fixed hours calculations

--- APBD_cw2_project_s27023/modules/quotable/rent/Rent.cs
+++ APBD_cw2_project_s27023/modules/quotable/rent/Rent.cs
@@ -56,7 +56,7 @@ public class Rent : Quotable
     {
         if (RealEnd is null) throw new RentNotEndedException();
 
-        return (int)(Start - RealEnd.Value).TotalHours + 1;
+        return Math.Max((int)(RealEnd.Value - Start).TotalHours, 0) + 1;
     }
 
     public bool WasHeldTooLong()
